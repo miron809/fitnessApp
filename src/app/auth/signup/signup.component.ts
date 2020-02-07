2a143e1812598d94e62eba35b7fc70ebba4f4676
Dmitriy Mironenko <dim@uain.su>
2020-02-07 17:54:42 +0200

    added signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.form = new FormGroup({
+      email: new FormControl(null, Validators.compose([Validators.required, Validators.email])),
+      password: new FormControl(null, Validators.compose([
+        Validators.required,
+        Validators.pattern['(?!\\s)^((?!\\s{1,}).)*$'],
+        Validators.min(6)])),
+    });
   }
 
 }
