75a80aefd2c8dd56daccfd329836e708e5c63f7c
Dmitriy Mironenko <dim@uain.su>
2020-02-18 16:42:55 +0200

    fix for environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBWRuV65a0VkIet5wMyHudVSy93dgedO3A',
+    authDomain: 'ng-fitness-tracker-91abb.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-91abb.firebaseio.com',
+    projectId: 'ng-fitness-tracker-91abb',
+    storageBucket: 'ng-fitness-tracker-91abb.appspot.com',
+    messagingSenderId: '791422941033',
+    appId: '1:791422941033:web:8b14999be001506c112ff8'
+  }
 };
