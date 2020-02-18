bb2b6fc3b96879157c9e4b958e5cf28827e6a4ed
Dmitriy Mironenko <dim@uain.su>
2020-02-18 16:13:11 +0200

    moved auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
-  { path: '' , component: WelcomeComponent},
-  { path: 'training', loadChildren: './training/training.module#TrainingModule' }
+  {path: '', component: WelcomeComponent},
+  {path: 'training', loadChildren: './training/training.module#TrainingModule', canLoad: [AuthGuard]}
 ];
 
 @NgModule({
@@ -14,4 +15,5 @@ const routes: Routes = [
   exports: [RouterModule],
   providers: [AuthGuard]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,16 +1,25 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService,
               private router: Router) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.authService.logout();
+      this.router.navigate(['/login']);
+    }
+  }
+
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
     if (this.authService.isAuth()) {
       return true;
     } else {
